1b5b4571ad2ebf80e4ef249c5be92a5ee663c95f
Daniel Haight <other+ecc@haight.io>
2021-09-30 18:41:59 +0100

    Building swift package

--- Package.swift
+++ Package.swift
@@ -2,14 +2,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "ZCashLC",
+    name: "ZcashLightClientKit",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
         .library(
-            name: "ZCashLC",
-            targets: ["ZCashLC"]
+            name: "ZcashLightClientKit",
+            targets: ["ZcashLightClientKit","ZCashLC"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0"),
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.13.0")
+    ],
     targets: [
+        .target(name: "ZcashLightClientKit", dependencies: [
+            .product(name: "SQLite", package: "SQLite.swift"),
+            .product(name: "GRPC", package: "grpc-swift"),
+        ]),
         .binaryTarget(name: "ZCashLC", path: "ZCashLC.xcframework")
     ]
 )
